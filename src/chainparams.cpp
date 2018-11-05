dbcf929e90582667e5f1ec8511540d3ca8300608
afghany <afghany@gmail.com>
2018-11-05 12:28:04 -0800

    added checkpoint

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -54,12 +54,13 @@ static void convertSeed6(std::vector<CAddress>& vSeedsOut, const SeedSpec6* data
 // + Contains no strange transactions
 static Checkpoints::MapCheckpoints mapCheckpoints =
     boost::assign::map_list_of
-    (0, uint256("00000da65c7ea599e637cdfaac58b37b9792da59e4cb2cc41f9ca0465a4387e3"))
+    (       0, uint256("00000da65c7ea599e637cdfaac58b37b9792da59e4cb2cc41f9ca0465a4387e3"))
+    (    2550, uint256("b65c8988dc018d10166e7fd55c697c6c42d1afccc0ffdc8d7188b921b6a987bb"))
     ;
 static const Checkpoints::CCheckpointData data = {
     &mapCheckpoints,
-    1536062400, // * UNIX timestamp of last checkpoint block
-    0,    // * total number of transactions between genesis and last checkpoint
+    1541436970, // * UNIX timestamp of last checkpoint block
+    4107,    // * total number of transactions between genesis and last checkpoint
                 //   (the tx=... number in the SetBestChain debug.log lines)
     2000        // * estimated number of transactions per day after checkpoint
 };
